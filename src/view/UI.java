b28ceecc64aaf385925e1d8a462648a64ab5b0c8
Tourn Vuthy <vuthytuon168@gmail.com>
2025-06-03 17:49:44 +0700

    Added: Table for listing Users and Products

--- src/view/UI.java
+++ src/view/UI.java
@@ -13,124 +13,139 @@ public class UI {
              = new ProductController();
     private static final UserController userController
             = new UserController();
-    private static void thumbnail(){
+    private static String selectEntity() {
+        System.out.println("===============");
+        System.out.println("  Select Type  ");
+        System.out.println("===============");
+        System.out.println("1. Product");
+        System.out.println("2. User");
+        System.out.print("[+] Choose: ");
+        int choice = new Scanner(System.in).nextInt();
+        return (choice == 1) ? "product" : "user";
+    }
+    private static void productMenu() {
         System.out.println("============================");
         System.out.println("      Product Inventory     ");
         System.out.println("============================");
         System.out.println("""
-                1. Get All Products
-                2. Add New Product 
-                3. Update Product 
-                4. Delete Product
-                5. Find Product
-                """);
+            1. Get All Products
+            2. Add New Product 
+            3. Update Product 
+            4. Delete Product
+            5. Find Product
+            0. Back
+            """);
+    }
+    private static void userMenu() {
         System.out.println("============================");
         System.out.println("       User Inventory       ");
         System.out.println("============================");
         System.out.println("""
-                6. Get All Users
-                7. Add New User
-                8. Update User
-                9. Delete User
-                10. Find User
-                0. Exit 
-                """);
+            1. Get All Users
+            2. Add New User
+            3. Update User
+            4. Delete User
+            5. Find User
+            0. Back
+            """);
     }
+    public static void home() {
+        while (true) {
+            String entity = selectEntity();
+            boolean goBack = false;
+            while (!goBack) {
+                if (entity.equals("product")) {
+                    productMenu();
+                } else {
+                    userMenu();
+                }
 
-    public static void home(){
-        while (true){
-            thumbnail();
-            System.out.print("[+] Insert option: ");
-            switch (new Scanner(System.in).nextInt()){
-                case 1->{
-                    productController
-                            .getAllProducts()
-                            .forEach(System.out::println);
-                }
-                case 2->{
-                    System.out.print("[+] Insert Product Name: ");
-                    String pName = new Scanner(System.in).nextLine();
-                    System.out.print("[+] Insert Expire Year: ");
-                    int year = new Scanner(System.in).nextInt();
-                    System.out.print("[+] Insert Expire Month: ");
-                    int month = new Scanner(System.in).nextInt();
-                    System.out.print("[+] Insert Expire Day: ");
-                    int day = new Scanner(System.in).nextInt();
-                    ProductCreateDto productCreateDto
-                             = new ProductCreateDto(pName, Date.valueOf(LocalDate.of(year, month, day)));
-                    ProductResponseDto product = productController.insertNewProduct(productCreateDto);
-                    System.out.println(product);
-                }
-                case 3->{
-                    System.out.print("[+] Insert Product Uuid: ");
-                    String uuid = new Scanner(System.in).nextLine();
-                    System.out.print("[+] Insert new Product Name: ");
-                    String newPName = new Scanner(System.in).nextLine();
-                    System.out.print("[+] Insert new Expired Year: ");
-                    int year = new Scanner(System.in).nextInt();
-                    System.out.print("[+] Insert new Expired Month: ");
-                    int month = new Scanner(System.in).nextInt();
-                    System.out.print("[+] Insert new Expired Day: ");
-                    int day = new Scanner(System.in).nextInt();
-                    ProductResponseDto updatedProduct = productController
-                            .updateProductByUuid(uuid,
-                                    new UpdateProductDto(newPName, Date.valueOf(LocalDate.of(year, month, day))));
-                    System.out.println(updatedProduct);
-                }
-                case 4->{
-                    System.out.print("[+] Delete Product By Uuid: ");
-                    String uuid = new Scanner(System.in).nextLine();
-                    productController.deleteProductByUuid(uuid);
-                    System.out.println("[+] Deleted successfully");
-                }
-                case 5->{
-                    System.out.print("[+] Find Product By Uuid: ");
-                    String uuid = new Scanner(System.in).nextLine();
-                    System.out.println(productController.getProductByUuid(uuid));
-                }
-                case 6->{
-                    userController
-                            .getAllUsers()
-                            .forEach(System.out::println);
-                }
-                case 7->{
-                    System.out.print("[+] Insert userName: ");
-                    String userName = new Scanner(System.in).nextLine();
-                    System.out.print("[+] Insert Email: ");
-                    String email = new Scanner(System.in).nextLine();
-                    System.out.print("[+] Insert Password: ");
-                    String password = new Scanner(System.in).nextLine();
-                    UserCreateDto userCreateDto
-                            = new UserCreateDto(userName,email,password);
-                    UserResponseDto user = userController.insertNewUser(userCreateDto);
-                    System.out.println(user);
-                }
-                case 8->{
-                    System.out.print("[+] Insert User Uuid: ");
-                    String uuid = new Scanner(System.in).nextLine();
-                    System.out.print("[+] Insert new Username: ");
-                    String newUserName = new Scanner(System.in).nextLine();
-                    UserResponseDto updatedUser = userController
-                            .updateUserByUuid(uuid,
-                                    new UpdateUserDto(newUserName));
-                    System.out.println(updatedUser);
-                }
-                case 9->{
-                    System.out.print("[+] Delete User By Uuid: ");
-                    String uuid = new Scanner(System.in).nextLine();
-                    userController.deleteUserByUuid(uuid);
-                    System.out.println("[+] Deleted successfully");
-                }
-                case 10->{
-                    System.out.print("[+] Find User By Uuid: ");
-                    String uuid = new Scanner(System.in).nextLine();
-                    System.out.println(userController.getUserByUuid(uuid));
-                }
-                case 0->{
-                    System.out.println("Thank you for using our system");
-                    return;
+                System.out.print("[+] Choose option: ");
+                int option = new Scanner(System.in).nextInt();
+
+                switch (option) {
+                    case 1 -> {
+                        if (entity.equals("product")) {
+                           new TableUI<ProductResponseDto>().getTableDisplay(productController.getAllProducts());
+                        } else {
+                            new TableUI<UserResponseDto>().getTableDisplay(userController.getAllUsers());
+                        }
+                    }
+                    case 2 -> {
+                        if (entity.equals("product")) {
+                            // Add new product
+                            System.out.print("[+] Insert Product Name: ");
+                            String pName = new Scanner(System.in).nextLine();
+                            System.out.print("[+] Insert Expire Year: ");
+                            int year = new Scanner(System.in).nextInt();
+                            System.out.print("[+] Insert Expire Month: ");
+                            int month = new Scanner(System.in).nextInt();
+                            System.out.print("[+] Insert Expire Day: ");
+                            int day = new Scanner(System.in).nextInt();
+                            ProductCreateDto productCreateDto = new ProductCreateDto(pName, Date.valueOf(LocalDate.of(year, month, day)));
+                            ProductResponseDto product = productController.insertNewProduct(productCreateDto);
+                            System.out.println(product);
+                        } else {
+                            // Add new user
+                            System.out.print("[+] Insert Username: ");
+                            String userName = new Scanner(System.in).nextLine();
+                            System.out.print("[+] Insert Email: ");
+                            String email = new Scanner(System.in).nextLine();
+                            System.out.print("[+] Insert Password: ");
+                            String password = new Scanner(System.in).nextLine();
+                            UserCreateDto userCreateDto = new UserCreateDto(userName, email, password);
+                            UserResponseDto user = userController.insertNewUser(userCreateDto);
+                            System.out.println(user);
+                        }
+                    }
+                    case 3 -> {
+                        if (entity.equals("product")) {
+                            System.out.print("[+] Insert Product UUID: ");
+                            String uuid = new Scanner(System.in).nextLine();
+                            System.out.print("[+] Insert new Product Name: ");
+                            String newName = new Scanner(System.in).nextLine();
+                            System.out.print("[+] Insert new Expire Year: ");
+                            int year = new Scanner(System.in).nextInt();
+                            System.out.print("[+] Insert new Expire Month: ");
+                            int month = new Scanner(System.in).nextInt();
+                            System.out.print("[+] Insert new Expire Day: ");
+                            int day = new Scanner(System.in).nextInt();
+                            UpdateProductDto dto = new UpdateProductDto(newName, Date.valueOf(LocalDate.of(year, month, day)));
+                            ProductResponseDto updatedProduct = productController.updateProductByUuid(uuid, dto);
+                            System.out.println(updatedProduct);
+                        } else {
+                            System.out.print("[+] Insert User UUID: ");
+                            String uuid = new Scanner(System.in).nextLine();
+                            System.out.print("[+] Insert new Username: ");
+                            String newName = new Scanner(System.in).nextLine();
+                            UpdateUserDto dto = new UpdateUserDto(newName);
+                            UserResponseDto updatedUser = userController.updateUserByUuid(uuid, dto);
+                            System.out.println(updatedUser);
+                        }
+                    }
+                    case 4 -> {
+                        System.out.print("[+] Insert UUID: ");
+                        String uuid = new Scanner(System.in).nextLine();
+                        if (entity.equals("product")) {
+                            productController.deleteProductByUuid(uuid);
+                        } else {
+                            userController.deleteUserByUuid(uuid);
+                        }
+                        System.out.println("[+] Deleted Successfully");
+                    }
+                    case 5 -> {
+                        System.out.print("[+] Insert UUID: ");
+                        String uuid = new Scanner(System.in).nextLine();
+                        if (entity.equals("product")) {
+                            System.out.println(productController.getProductByUuid(uuid));
+                        } else {
+                            System.out.println(userController.getUserByUuid(uuid));
+                        }
+                    }
+                    case 0 -> goBack = true;
                 }
             }
         }
     }
+
 }
